f9a1197e160d8ea8d6d88aa6b104c27c2faadc88
Николай <xenos-nick@mail.ru>
2024-02-03 19:44:44 +0300

    Обработка событий клавиатуры

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 
-function changeClick() {
+function submitForm() {
     const input = document.querySelector('.input').value;
     if (!input) {
         return;
     }
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
+}
+
+function inputChanged(e) {
+    if (e.code == 'Enter') {
+        submitForm();
+    }
 }
